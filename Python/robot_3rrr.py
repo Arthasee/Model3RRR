925046f3558972517bb847026810a5a050eeabce
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-04-09 11:35:23 +0200

    cleaning code

--- Python/robot_3rrr.py
+++ Python/robot_3rrr.py
@@ -1,6 +1,6 @@
 """test for a 3rrr class"""
 
-from numpy import pi, sin, cos, array, sqrt
+from numpy import pi, array
 import matplotlib.pyplot as plt
 import pygame
 import pygame.locals
@@ -10,6 +10,8 @@ from mgi_analytique import mgi_analytique
 
 
 class Robot3RRR:
+    """class of the robot 3RRR
+    """
     def __init__(self, l1=0.1, l2=0.1, rb=0.1322594, re=0.07):
         self.clock = [0.0]
         self.l1 = l1
@@ -32,62 +34,81 @@ class Robot3RRR:
         pass
 
     def draw(self, screen=None):
+        """draw the robot with matplotlib or pygame
+
+        Args:
+            screen (pygame.display, optional): the screen of pygame if used. Defaults to None.
+        """
         if not self.game:
-            f = trace_rob(self.q, 1)
+            trace_rob(self.q, 1)
             plt.show()
             return
 
-        p10, p11, p12, p20, p21, p22, p30, p31, p32 = trace_rob_game(self.q)
+        q = self.q
+        offset2 = array([self.game_dimensions[0], self.game_dimensions[1]])
+        offset3 = array([self.game_dimensions[0],
+                        self.game_dimensions[1],
+                        0])
+
+        p10, p11, p12, p20, p21, p22, p30, p31, p32 = trace_rob_game(q)
 
         p10 *= self.scale
-        p10 += array([self.game_dimensions[0], self.game_dimensions[1]])/2
+        p10 += offset2/2
 
         p11 *= self.scale
-        p11 += array([self.game_dimensions[0], self.game_dimensions[1], 0])/2
+        p11 += offset3/2
 
         p12 *= self.scale
-        p12 += array([self.game_dimensions[0], self.game_dimensions[1], 0])/2
+        p12 += offset3/2
 
         p20 *= self.scale
-        p20 += array([self.game_dimensions[0], self.game_dimensions[1]])/2
+        p20 += offset2/2
 
         p21 *= self.scale
-        p21 += array([self.game_dimensions[0], self.game_dimensions[1], 0])/2
+        p21 += offset3/2
 
         p22 *= self.scale
-        p22 += array([self.game_dimensions[0], self.game_dimensions[1], 0])/2
+        p22 += offset3/2
 
         p30 *= self.scale
-        p30 += array([self.game_dimensions[0], self.game_dimensions[1]])/2
+        p30 += offset2/2
 
         p31 *= self.scale
-        p31 += array([self.game_dimensions[0], self.game_dimensions[1], 0])/2
+        p31 += offset3/2
 
         p32 *= self.scale
-        p32 += array([self.game_dimensions[0], self.game_dimensions[1], 0])/2
+        p32 += offset3/2
 
-        l1 = pygame.draw.line(screen, (255, 0, 0), (p10[0], p10[1]), (p11[0], p11[1]), 2)
-        l2 = pygame.draw.line(screen, (255, 0, 255), (p11[0], p11[1]), (p12[0], p12[1]), 2)
+        l1 = pygame.draw.line(screen, (255, 0, 0),
+                              (p10[0], p10[1]), (p11[0], p11[1]), 2)
+        l2 = pygame.draw.line(screen, (255, 0, 255),
+                              (p11[0], p11[1]), (p12[0], p12[1]), 2)
 
-        l3 = pygame.draw.line(screen, (255, 0, 0), (p20[0], p20[1]), (p21[0], p21[1]), 2)
-        l4 = pygame.draw.line(screen, (255, 0, 255), (p21[0], p21[1]), (p22[0], p22[1]), 2)
+        l3 = pygame.draw.line(screen, (255, 0, 0),
+                              (p20[0], p20[1]), (p21[0], p21[1]), 2)
+        l4 = pygame.draw.line(screen, (255, 0, 255),
+                              (p21[0], p21[1]), (p22[0], p22[1]), 2)
 
-        l5 = pygame.draw.line(screen, (255, 0, 0), (p30[0], p30[1]), (p31[0], p31[1]), 2)
-        l6 = pygame.draw.line(screen, (255, 0, 255), (p31[0], p31[1]), (p32[0], p32[1]), 2)
+        l5 = pygame.draw.line(screen, (255, 0, 0),
+                              (p30[0], p30[1]), (p31[0], p31[1]), 2)
+        l6 = pygame.draw.line(screen, (255, 0, 255),
+                              (p31[0], p31[1]), (p32[0], p32[1]), 2)
 
-        triangle = pygame.draw.polygon(screen, (0, 255, 0), [(p12[0], p12[1]),
-                                                             (p22[0], p22[1]),
-                                                             (p32[0], p32[1])])
-
-        # return (l1, l2, l3, l4, l5, l6, triangle)
+        triangle = pygame.draw.polygon(screen,
+                                       (0, 255, 0),
+                                       [(p12[0], p12[1]),
+                                        (p22[0], p22[1]),
+                                        (p32[0], p32[1])])
 
     def simulate(self):
+        """simulate on pygame the robot and its displacements
+        """
         if not self.game:
             return
         successes, failures = pygame.init()
         screen = pygame.display.set_mode(self.game_dimensions)
         clock = pygame.time.Clock()
-        font = pygame.font.Font(None, 36)
+        # font = pygame.font.Font(None, 36)
 
         running = True
 
@@ -99,9 +120,9 @@ class Robot3RRR:
             screen.fill((255, 255, 255))
             self.draw(screen)
 
-            time_text = font.render(f"Time: {round(self.clock[-1], 1)}", True, (0, 0, 0))
-            text_rect = time_text.get_rect(bottomright=(self.game_dimensions[0] - 10, self.game_dimensions[1] - 10))
-            screen.blit(time_text, text_rect)
+            # time_text = font.render(f"Time: {round(self.clock[-1], 1)}", True, (0, 0, 0))
+            # text_rect = time_text.get_rect(bottomright=(self.game_dimensions[0] - 10, self.game_dimensions[1] - 10))
+            # screen.blit(time_text, text_rect)
 
             pygame.display.flip()
 
@@ -112,6 +133,7 @@ class Robot3RRR:
 
 if __name__ == '__main__':
     robot = Robot3RRR()
-    robot.game = True
+    # robot.game = True
+    robot.draw()
     robot.q = mgi_analytique(robot.pos_eff)
     robot.simulate()
