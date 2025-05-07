c73aa885592498b6d1da2d26da74447538f44483
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-05-07 15:06:25 +0200

    add trace

--- Python/robot_3rrr.py
+++ Python/robot_3rrr.py
@@ -1,11 +1,11 @@
 """test for a 3rrr class"""
 
-from numpy import pi, array
+from numpy import pi, array, cos, sin, tan, sqrt
 import matplotlib.pyplot as plt
 import pygame
 import pygame.locals
 
-from trace_rob import trace_rob, trace_rob_game
+# from trace_rob import trace_rob, trace_rob_game
 from mgi_analytique import mgi_analytique
 
 
@@ -33,6 +33,121 @@ class Robot3RRR:
 
     def __repr__(self):
         return str(self)
+    
+    def trace_rob(self, q, name):
+        """trace the robot with matplotlib.pyplot
+
+        Args:
+            q (list): angle values of the 3 arms
+            name (int or str): the name (or number) of the figure to differentiate\
+            methods
+
+        Returns:
+            matplot.pyplot.figure: the figure with the robot
+        """
+        alpha1 = q[0]
+        beta1 = q[1]
+        alpha2 = q[2]
+        beta2 = q[3]
+        alpha3 = q[4]
+        beta3 = q[5]
+
+        rot1 = array([[cos(2*pi/3), -sin(2*pi/3)], [sin(2*pi/3), cos(2*pi/3)]])
+        rot2 = array([[cos(4*pi/3), -sin(4*pi/3)], [sin(4*pi/3), cos(4*pi/3)]])
+
+        p10 = array([0, -self.rb])
+        first_row1 = array([[1, 0, 0], [0, 1, -self.rb], [0, 0, 1]])
+        p11 = first_row1.dot(array([self.l1*cos(alpha1), self.l1*sin(alpha1), 1]))
+        p12 = first_row1.dot(array([self.l1*cos(alpha1)+self.l2*cos(alpha1+beta1),
+                                    self.l1*sin(alpha1)+self.l2*sin(alpha1+beta1),
+                                    1]))
+
+        p20 = array([self.rb*sqrt(3)/2, self.rb/2])
+        first_row2 = array([[rot1[0][0], rot1[0][1], p20[0]],
+                            [rot1[1][0], rot1[1][1], p20[1]]])
+        p21 = array([first_row2[0],
+                    first_row2[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha2), self.l1*sin(alpha2), 1]))
+        p22 = array([first_row2[0],
+                    first_row2[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha2)+self.l2*cos(alpha2+beta2),
+                                            self.l1*sin(alpha2)+self.l2*sin(alpha2+beta2),
+                                            1]))
+
+        p30 = array([-self.rb*sqrt(3)/2, self.rb/2])
+        first_row3 = array([[rot2[0][0], rot2[0][1], p30[0]],
+                            [rot2[1][0], rot2[1][1], p30[1]]])
+        p31 = array([first_row3[0],
+                    first_row3[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha3), self.l1*sin(alpha3), 1]))
+        p32 = array([first_row3[0],
+                    first_row3[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha3)+self.l2*cos(alpha3+beta3),
+                                            self.l1*sin(alpha3)+self.l2*sin(alpha3+beta3),
+                                            1]))
+
+        f = plt.figure(name)
+        plt.plot([p10[0], p11[0], p12[0]], [p10[1], p11[1], p12[1]])
+        plt.plot([p20[0], p21[0], p22[0]], [p20[1], p21[1], p22[1]])
+        plt.plot([p30[0], p31[0], p32[0]], [p30[1], p31[1], p32[1]])
+        plt.plot([p12[0], p22[0], p32[0], p12[0]],
+                [p12[1], p22[1], p32[1], p12[1]], linewidth=2)
+        plt.axis("square")
+        plt.axis("equal")
+        # plt.show()
+        return f
+
+    def trace_rob_game(self, q):
+        """trace the robot with pygame
+
+        Args:
+            q (list): angle values of the 3 arms
+
+        Returns:
+            pygame.draw: the figure with the robot
+        """
+        alpha1 = q[0]
+        beta1 = q[1]
+        alpha2 = q[2]
+        beta2 = q[3]
+        alpha3 = q[4]
+        beta3 = q[5]
+
+        rot1 = array([[cos(2*pi/3), -sin(2*pi/3)], [sin(2*pi/3), cos(2*pi/3)]])
+        rot2 = array([[cos(4*pi/3), -sin(4*pi/3)], [sin(4*pi/3), cos(4*pi/3)]])
+
+        p10 = array([0, -self.rb])
+        first_row1 = array([[1, 0, 0], [0, 1, -self.rb], [0, 0, 1]])
+        p11 = first_row1.dot(array([self.l1*cos(alpha1), self.l1*sin(alpha1), 1]))
+        p12 = first_row1.dot(array([self.l1*cos(alpha1)+self.l2*cos(alpha1+beta1),
+                                    self.l1*sin(alpha1)+self.l2*sin(alpha1+beta1),
+                                    1]))
+
+        p20 = array([self.rb*sqrt(3)/2, self.rb/2])
+        first_row2 = array([[rot1[0][0], rot1[0][1], p20[0]],
+                            [rot1[1][0], rot1[1][1], p20[1]]])
+        p21 = array([first_row2[0],
+                    first_row2[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha2), self.l1*sin(alpha2), 1]))
+        p22 = array([first_row2[0],
+                    first_row2[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha2)+self.l2*cos(alpha2+beta2),
+                                            self.l1*sin(alpha2)+self.l2*sin(alpha2+beta2),
+                                            1]))
+
+        p30 = array([-self.rb*sqrt(3)/2, self.rb/2])
+        first_row3 = array([[rot2[0][0], rot2[0][1], p30[0]],
+                            [rot2[1][0], rot2[1][1], p30[1]]])
+        p31 = array([first_row3[0],
+                    first_row3[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha3), self.l1*sin(alpha3), 1]))
+        p32 = array([first_row3[0],
+                    first_row3[1],
+                    [0, 0, 1]]).dot(array([self.l1*cos(alpha3)+self.l2*cos(alpha3+beta3),
+                                            self.l1*sin(alpha3)+self.l2*sin(alpha3+beta3),
+                                            1]))
+
+        return p10, p11, p12, p20, p21, p22, p30, p31, p32
 
     def draw(self, screen=None):
         """draw the robot with matplotlib or pygame
@@ -42,7 +157,7 @@ class Robot3RRR:
                 Defaults to None.
         """
         if not self.game:
-            trace_rob(self.q, 1)
+            self.trace_rob(self.q, 1)
             plt.show()
             return
 
@@ -52,7 +167,7 @@ class Robot3RRR:
                         self.game_dimensions[1],
                         0])
 
-        p10, p11, p12, p20, p21, p22, p30, p31, p32 = trace_rob_game(q)
+        p10, p11, p12, p20, p21, p22, p30, p31, p32 = self.trace_rob_game(q)
 
         p10 *= self.scale
         p10 += offset2/2
@@ -184,5 +299,6 @@ if __name__ == '__main__':
     robot.game = True
     print(robot)
     robot.q = mgi_analytique(robot.pos_eff)
-    # robot.draw()
     robot.simulate()
+    robot.game = False
+    robot.draw()
